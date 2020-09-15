4b0a7c7dd6d91a5baf35dfaebcc90edde584c835
nkaplan89 <70921188+nkaplan89@users.noreply.github.com>
2020-09-15 01:19:54 -0400

    cvstuff

---
+++ app.js
@@ -0,0 +1,16 @@
+const navSlide =() => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLink = document.querySelectorAll('.nav-links li');
+    burger.addEventListener('click',() => {
+        nav.classList.toggle('nav-active');
+    });
+
+    navLinks.forEach((link,index) => {
+        link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 3}s';
+    
+        
+    });
+}
+
+navSlide();
